4793f81acccdf1495c85f6cbaf4b64ba37242169
Alex <alexbujduveanu@gmail.com>
2017-07-15 11:57:10 -0400

    Add parsing for active tor nodes

--- threat_intel.py
+++ threat_intel.py
@@ -4,7 +4,8 @@ from netaddr import IPNetwork, IPAddress
 
 # global dict with URLs for each feed
 FEEDS = {
-    'spamhaus': 'https://www.spamhaus.org/drop/drop.txt'
+    'spamhaus': 'https://www.spamhaus.org/drop/drop.txt',
+    'tor': 'https://check.torproject.org/exit-addresses'
 }
 
 def check_spamhaus(ip_string):
@@ -32,11 +33,44 @@ def check_spamhaus(ip_string):
         print('[-] Error encountered in Spamhaus feed function ' + str(e))
         return False
 
+def check_tor(ip_string):
+    exit_node_list = []
+    try:
+        url = FEEDS['tor']
+        resp = urllib.request.urlopen(url)
+        data = resp.read().decode('utf-8')
+
+        # build list of current tor IPs
+        for line in data.splitlines():
+            if('ExitAddress' in line):
+                ip = line.split(' ')[1]
+                exit_node_list.append(ip)
+
+        if(ip in exit_node_list):
+            print('[+] IP is a tor exit node')
+            return True
+        else:
+            return False
+    except Exception as e:
+        print('[-] Error in tor feed function')
+        print(str(e))
+        return False
+
 def test_check_spamhaus():
     ip_to_test = '192.203.252.1'
+
     if(check_spamhaus(ip_to_test)):
         print('[+] Spamhaus threat feed test passed')
     else:
         print('[-] Spamhaus threat feed test failed')
 
-test_check_spamhaus()
+def test_check_tor():
+    ip_to_test = '185.165.168.186'
+
+    if(check_tor(ip_to_test)):
+        print('[+] Tor feed test passed')
+    else:
+        print('[-] Tor feed test failed')
+
+# test_check_spamhaus()
+# test_check_tor()
